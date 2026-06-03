d79a3bd7d561b41014d0b3e33f2582edb666e4be
oikawatoru01 <julia.vitoria.nunes@escola.pr.gov.br>
2026-06-03 13:53:31 +0000

    versao1

--- animal.js
+++ animal.js
@@ -1,1 +1,10 @@
+const animal = {
+    nome: "Sofia", //string
+    especie: "Cachorro",
+    idade: 8, //number
+    tutor: false,
+    vacinado: true
+};
 
+//console.log(animal);
+console.log(animal.nome);
