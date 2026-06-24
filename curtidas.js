6633aaefa1f81b444f8cead99526410bb12445d7
oikawatoru01 <julia.vitoria.nunes@escola.pr.gov.br>
2026-06-24 13:48:36 +0000

    v1

---
+++ curtidas.js
@@ -0,0 +1,18 @@
+const feedJSON = require('./feed.json');
+
+//Adicionar uma curtida oa post pelo id
+function curtir(feed, id) {
+    const post = feed[0].id === id ? feed[0]; 
+}
+
+if (!post) {
+    console.log("Post nao encontrado.")
+    return;
+}
+
+if(!post.publicado ) {
+    console.log("Não é possível curtir um rascunho.");
+    return;
+}
+
+post.metricas.curtidas++;
